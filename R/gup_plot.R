8fe29037a94a428b12e4aa74416ce8fcb44dd3af
cchz <3330975656@qq.com>
2024-03-27 16:41:16 +0800

    plot renewn

--- R/gup_plot.R
+++ R/gup_plot.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello'
+# This is an example function named 'gup_plot'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -14,19 +14,25 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 
-gup_plot <- function(id="002468.sz",day=30) {
+gup_plot <- function(id="002468.sz",day=30,size=2) {
+  # id="002468.sz";day=30;size=2
   library(tidyverse)
   library(pedquant)
   library(plotly)
-  stock<-md_stock(id,from=today()-day,
+  library(zoo)
+  stock<-md_stock(id,from=today()-day-5,
                   to=today(),
                   source="163",
                   adjust="dividend")
   mydata<-stock[[1]]%>%as_tibble() %>%
-    mutate(date=ymd(date),xx=c(1:length(date)))
-  xdat<-mydata[seq(1,nrow(mydata),30),c('xx',"date")]
-  p1<-ggplot()+geom_line(data=mydata,aes(x=xx,y=close))+
-    theme_classic()
+    mutate(mean_5day=zoo::rollmean(amount,k = 5, align = "right", fill = NA)/
+             zoo::rollmean(volume, k = 5,align = "right", fill = NA)/100)
+  p1<-ggplot(data=mydata,aes(x=date,y=close))+
+    geom_line()+
+    geom_point(aes(col=volume),size=size,show.legend = F)+
+    geom_line(aes(y=mean_5day),col='red',alpha=0.6)+
+    scale_color_gradient2(mid="green",high = 'red')+
+    theme_dark()
   ggplotly(p1)
 }
 
